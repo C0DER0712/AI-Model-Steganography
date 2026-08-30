11b34ddfb351fc7aee9a47edb36bd733a9a11024
C0DER0712 <mohamedamjad0712@gmail.com>
2026-08-31 00:04:56 +0530

    Removed zip_to_pt.py

--- zip_to_pt.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-# Try loading the zip file directly
-checkpoint = torch.load("C:/Users/Amjad/Downloads/best_6500.zip", map_location='cpu')
-
-# Resave it explicitly as a .pt file
-torch.save(checkpoint, 'C:/Users/Amjad/Downloads/best_6500.pt')
